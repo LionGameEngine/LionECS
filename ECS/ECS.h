15290c197e74721f1ee6657da410ef35d9ab7f05
Tomasz Lewandowski <tomasz.t.lewandowski@gmail.com>
2020-01-01 02:46:16 +0100

    initial ecs target

---
+++ ECS/ECS.h
@@ -0,0 +1,18 @@
+//
+//  ECS.h
+//  ECS
+//
+//  Created by Tomasz Lewandowski on 01/01/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ECS.
+FOUNDATION_EXPORT double ECSVersionNumber;
+
+//! Project version string for ECS.
+FOUNDATION_EXPORT const unsigned char ECSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ECS/PublicHeader.h>
+
+
