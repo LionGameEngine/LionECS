183f114a0ee0d9a9ed75f1ec7133ea262712a460
Tomasz Lewandowski <tomasz.t.lewandowski@gmail.com>
2020-01-05 15:04:54 +0100

    prototype builder tests

---
+++ ECSTests/PrototypeBuilderTests.swift
@@ -0,0 +1,69 @@
+//
+//  PrototypeBuilderTests.swift
+//  Example
+//
+//  Created by Tomasz Lewandowski on 05/01/2020.
+//  Copyright © 2020 LionSoftware. All rights reserved.
+//
+
+import XCTest
+@testable import LionECS
+
+class PrototypeBuilderTests: XCTestCase {
+    func testBuild_WhenJustInitialized_ShouldReturnPrototypeWithoutComponentIdentifiers() {
+        // given
+        let sut = PrototypeBuilder()
+        // then
+        XCTAssertEqual([], sut.build().componentIdentifiers)
+    }
+    
+    func testBuild_WhenJustInitializedWithPrototype_ShouldReturnPrototypeWithSameIdentifiers() {
+        // given
+        let prototype = Prototype(componentIdentifiers: [PComponentMock.componentIdentifier])
+        let sut = PrototypeBuilder(prototype: prototype)
+        // then
+        XCTAssertEqual(prototype.componentIdentifiers, sut.build().componentIdentifiers)
+    }
+    
+    func testBuild_WhenJustInitializedWithPrototype_ShouldReturnNewInstanceOfPrototype() {
+        // given
+        let prototype = Prototype(componentIdentifiers: [PComponentMock.componentIdentifier])
+        let sut = PrototypeBuilder(prototype: prototype)
+        // then
+        XCTAssertFalse(prototype === sut.build())
+    }
+        
+    func testAdd_WhenCalled_ShouldAddGivenComponentIdentifier() {
+        // when
+        let sut = PrototypeBuilder().addComponentType(PComponentMock.self)
+        // then
+        XCTAssertEqual([PComponentMock.componentIdentifier], sut.build().componentIdentifiers)
+    }
+    
+    func testAdd_WhenCalledMoreThanOnce_ShouldAddComponentIdentifierJustOnce() {
+        // when
+        let sut = PrototypeBuilder().addComponentType(PComponentMock.self)
+            .addComponentType(PComponentMock.self)
+            .addComponentType(PComponentMock.self)
+        // then
+        XCTAssertEqual([PComponentMock.componentIdentifier], sut.build().componentIdentifiers)
+    }
+    
+    func testRemove_WhenCalled_ShouldRemoveGivenComponentIdentifier() {
+        // given
+        var sut = PrototypeBuilder().addComponentType(PComponentMock.self)
+        // when
+        sut = sut.removeComponentType(PComponentMock.self)
+        // then
+        XCTAssertEqual([], sut.build().componentIdentifiers)
+    }
+    
+    func testRemove_WhenCalledForEmptyBuilder_ShouldDoNothing() {
+        // given
+        var sut = PrototypeBuilder()
+        // when
+        sut = sut.removeComponentType(PComponentMock.self)
+        // then
+        XCTAssertEqual([], sut.build().componentIdentifiers)
+    }
+}
