0bc2286d755dac96e8cd7f53fe2c14ab91bfe331
Tomasz Lewandowski <tomasz.t.lewandowski@gmail.com>
2020-04-11 16:02:14 +0200

    get rid of GoL example

---
+++ Example/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Example
+//
+//  Created by Tomasz Lewandowski on 11/04/2020.
+//  Copyright © 2020 Lion Software Tomasz Lewandowski. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
